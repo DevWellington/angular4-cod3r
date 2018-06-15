b4f1ae9cd3c347aa60e6328fc2515ed6ff1436d8
= <devwellington@gmail.com>
2018-06-15 20:59:02 -0300

    Funcoes, Classes e interfaces com TypeScript

--- typescript/app.ts
+++ typescript/app.ts
@@ -11,3 +11,56 @@ console.log("Next episode is " + episode)
 let favoriteDroid: any
 favoriteDroid = "K-2SO"
 console.log("My favorite droid is " + favoriteDroid)
+
+
+let isEnoughToBeatMF = function (parsecs: number): boolean {
+	return parsecs < 12
+}
+
+let distance = 11
+
+console.log(`Is ${distance} parsecs enough to beat Millennium Falcon ${isEnoughToBeatMF(distance) ? 'YES' : 'NO'} `)
+
+// array functions
+let call = (name: string) => console.log(`Do you copy, ${name}?`)
+call('R2')
+
+
+// parametros padroes em TS
+function inc (speed: number, inc: number = 1): number {
+	return speed + inc
+}
+
+console.log(`ìnc (5,1) = ${inc(5,1)}`)
+console.log(`ìnc (5) = ${inc(5)}`)
+
+// Classes e Interfaces
+class Spacecraft {
+	
+	constructor(public propulsor: string) {}
+
+	jumpIntoHyperspace() {
+		console.log(`Entering hyperspace with ${this.propulsor}`)
+	}
+}
+
+let ship = new Spacecraft('hyperdrive')
+ship.jumpIntoHyperspace()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- typescript/dist/app.js
+++ typescript/dist/app.js
@@ -8,3 +8,30 @@ console.log("Next episode is " + episode);
 var favoriteDroid;
 favoriteDroid = "K-2SO";
 console.log("My favorite droid is " + favoriteDroid);
+var isEnoughToBeatMF = function (parsecs) {
+    return parsecs < 12;
+};
+var distance = 11;
+console.log("Is " + distance + " parsecs enough to beat Millennium Falcon " + (isEnoughToBeatMF(distance) ? 'YES' : 'NO') + " ");
+// array functions
+var call = function (name) { return console.log("Do you copy, " + name + "?"); };
+call('R2');
+// parametros padroes em TS
+function inc(speed, inc) {
+    if (inc === void 0) { inc = 1; }
+    return speed + inc;
+}
+console.log("\u00ECnc (5,1) = " + inc(5, 1));
+console.log("\u00ECnc (5) = " + inc(5));
+// Classes e Interfaces
+var Spacecraft = /** @class */ (function () {
+    function Spacecraft(propulsor) {
+        this.propulsor = propulsor;
+    }
+    Spacecraft.prototype.jumpIntoHyperspace = function () {
+        console.log("Entering hyperspace with " + this.propulsor);
+    };
+    return Spacecraft;
+}());
+var ship = new Spacecraft('hyperdrive');
+ship.jumpIntoHyperspace();
