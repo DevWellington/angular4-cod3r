3edd2652f415f78abdb181c1fda2788f8ffbf679
= <devwellington@gmail.com>
2018-06-16 15:52:12 -0300

    Usando Classes e Interfaces

--- typescript/app.ts
+++ typescript/app.ts
@@ -47,15 +47,35 @@ class Spacecraft {
 let ship = new Spacecraft('hyperdrive')
 ship.jumpIntoHyperspace()
 
+class MilleniumFalcon extends Spacecraft implements Containership {
+
+	cargoContainers: number
+
+	constructor (){
+		super('hiperdrive')
+		this.cargoContainers = 4
+	}
+
+	jumpIntoHyperspace(){
+		if (Math.random() >= 0.5 ){
+			super.jumpIntoHyperspace()
+		} else {
+			console.log('Failed to jump into hyperspace')
+		}
+	}
+}
+
+let falcon = new MilleniumFalcon()
+falcon.jumpIntoHyperspace()
 
 
+interface Containership {
+	cargoContainers: number
+}
 
+let goodForTheJob = ( ship: Containership ) => ship.cargoContainers > 2
 
-
-
-
-
-
+console.log(`Is falcon good for the job? ${goodForTheJob (falcon) ? 'yes' : 'no' }`)
 
 
 
--- typescript/dist/app.js
+++ typescript/dist/app.js
@@ -1,3 +1,13 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var message = "Help me, Obi-Wan Kenobi. You're my only hope!";
 console.log(message);
 var episode = 4;
@@ -35,3 +45,24 @@ var Spacecraft = /** @class */ (function () {
 }());
 var ship = new Spacecraft('hyperdrive');
 ship.jumpIntoHyperspace();
+var MilleniumFalcon = /** @class */ (function (_super) {
+    __extends(MilleniumFalcon, _super);
+    function MilleniumFalcon() {
+        var _this = _super.call(this, 'hiperdrive') || this;
+        _this.cargoContainers = 4;
+        return _this;
+    }
+    MilleniumFalcon.prototype.jumpIntoHyperspace = function () {
+        if (Math.random() >= 0.5) {
+            _super.prototype.jumpIntoHyperspace.call(this);
+        }
+        else {
+            console.log('Failed to jump into hyperspace');
+        }
+    };
+    return MilleniumFalcon;
+}(Spacecraft));
+var falcon = new MilleniumFalcon();
+falcon.jumpIntoHyperspace();
+var goodForTheJob = function (ship) { return ship.cargoContainers > 2; };
+console.log("Is falcon good for the job? " + (goodForTheJob(falcon) ? 'yes' : 'no'));
