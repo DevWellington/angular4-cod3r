4868316d09dcd20f0ee48c1d0f67a62f129e7e05
= <devwellington@gmail.com>
2018-06-18 20:55:52 -0300

    Diretivas *ngIf e *ngFor

--- angular-projects/jedi-academy/src/app/app.component.ts
+++ angular-projects/jedi-academy/src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 
+import { Student } from './student/student.model'
+
 @Component({
   selector: 'jad-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  luke = { name: 'Luke', isJedi: true, temple: 'Coruscant' }
-  leia = { name: 'Leia', isJedi: false }
-  han = { name: 'Han Solo', isJedi: false }
+
+  students: Student[] = [
+  	{ name: 'Luke', isJedi: true, temple: 'Coruscant' },
+  	{ name: 'Leia', isJedi: false },
+  	{ name: 'Han Solo', isJedi: false }
+  ]
+
 }
