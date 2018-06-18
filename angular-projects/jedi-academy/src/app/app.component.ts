35f3d9161f51274fda6ba68b5a92bcd72b68219d
= <devwellington@gmail.com>
2018-06-18 20:44:15 -0300

    Usando o decorator @Input

--- angular-projects/jedi-academy/src/app/app.component.ts
+++ angular-projects/jedi-academy/src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'jad';
+  luke = { name: 'Luke', isJedi: true, temple: 'Coruscant' }
+  leia = { name: 'Leia', isJedi: false }
+  han = { name: 'Han Solo', isJedi: false }
 }
