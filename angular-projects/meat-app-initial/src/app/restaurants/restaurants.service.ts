e931fd0bb3c9c9d747467a9d29f4e175ca28edb0
= <devwellington@gmail.com>
2018-06-26 22:29:31 -0300

    Tratamento de erros com

--- angular-projects/meat-app-initial/src/app/restaurants/restaurants.service.ts
+++ angular-projects/meat-app-initial/src/app/restaurants/restaurants.service.ts
@@ -4,9 +4,12 @@ import { Http } from '@angular/http'
 
 import { Observable } from 'rxjs/Observable'
 import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/catch'
+
 import { Restaurant } from './restaurant/restaurant.model'
 
 import { MEAT_API } from '../app.api'
+import { ErrorHandler } from '../app.error-handler'
 
 @Injectable()
 
@@ -15,9 +18,10 @@ export class RestaurantsService {
   constructor(private http: Http){}
 
   restaurants(): Observable<Restaurant[]> {
-  	return this.http
+    return this.http
       .get(`${MEAT_API}/restaurants`)
       .map(response => response.json())
+      .catch(ErrorHandler.handlerError)
   }
 
 }
