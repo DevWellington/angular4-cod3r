81a00ffb77f83420f737d84b74e754c1cc81d20a
= <devwellington@gmail.com>
2018-06-30 00:40:02 -0300

    Alterando localizacao da moeda para pt-BR

--- angular-projects/meat-app-initial/src/app/app.module.ts
+++ angular-projects/meat-app-initial/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 
@@ -40,7 +40,14 @@ import { ShoppingCartService } from './restaurant-detail/shopping-cart/shopping-
     HttpModule,
     RouterModule.forRoot(ROUTES)
   ],
-  providers: [RestaurantsService, ShoppingCartService],
+  providers: [
+    RestaurantsService, 
+    ShoppingCartService,
+    {
+      provide: LOCALE_ID, 
+      useValue: 'pt-BR'
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
