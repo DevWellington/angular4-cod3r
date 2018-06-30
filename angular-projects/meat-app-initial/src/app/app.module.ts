4b85779f37d25c106ea266120f46e39e28fd77cd
= <devwellington@gmail.com>
2018-06-30 00:22:55 -0300

    Registrando service e ngIf

--- angular-projects/meat-app-initial/src/app/app.module.ts
+++ angular-projects/meat-app-initial/src/app/app.module.ts
@@ -19,6 +19,8 @@ import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shoppin
 import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component';
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component'
 
+import { ShoppingCartService } from './restaurant-detail/shopping-cart/shopping-cart.service'
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -38,7 +40,7 @@ import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component'
     HttpModule,
     RouterModule.forRoot(ROUTES)
   ],
-  providers: [RestaurantsService],
+  providers: [RestaurantsService, ShoppingCartService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- angular-projects/meat-app-initial/src/app/restaurant-detail/shopping-cart/shopping-cart.component.ts
+++ angular-projects/meat-app-initial/src/app/restaurant-detail/shopping-cart/shopping-cart.component.ts
@@ -1,14 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ShoppingCartService } from './shopping-cart.service'
+
 @Component({
   selector: 'mt-shopping-cart',
   templateUrl: './shopping-cart.component.html'
 })
 export class ShoppingCartComponent implements OnInit {
 
-  constructor() { }
+	// shoppingCartService: ShoppingCartService
+
+  constructor(
+  	private shoppingCartService: ShoppingCartService
+  ) { }
 
   ngOnInit() {
+  }
+
+  items(): any[] {
+  	return this.shoppingCartService.items
+  }
+
+  total(): number {
+  	return this.shoppingCartService.total()
   }
 
 }
