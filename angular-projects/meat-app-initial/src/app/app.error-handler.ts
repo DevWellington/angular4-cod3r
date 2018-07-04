e6d967bfa2d2a27f28cb854a95965fe1535bd415
= <devwellington@gmail.com>
2018-07-03 21:38:38 -0300

    Arrow Function diff example

--- angular-projects/meat-app-initial/src/app/app.error-handler.ts
+++ angular-projects/meat-app-initial/src/app/app.error-handler.ts
@@ -2,7 +2,7 @@ import { Response } from '@angular/http'
 import { Observable } from 'rxjs/Observable'
 
 export class ErrorHandler {
-	static handlerError(error: Response | any){
+	static handlerError = (error: Response | any) => {
 		let errorMessage: string
 
 		if (error instanceof Response){
