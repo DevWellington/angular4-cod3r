7cec7440837bb46cd391c6a996c41e4ff8b5c62f
= <devwellington@gmail.com>
2018-07-05 20:57:24 -0300

    Refatorando com components

--- angular-projects/meat-app-initial/src/app/app.module.ts
+++ angular-projects/meat-app-initial/src/app/app.module.ts
@@ -21,7 +21,8 @@ import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.compo
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component'
 
 import { ShoppingCartService } from './restaurant-detail/shopping-cart/shopping-cart.service';
-import { OrderComponent } from './order/order.component'
+import { OrderComponent } from './order/order.component';
+import { InputComponent } from './shared/input/input.component'
 
 @NgModule({
   declarations: [
@@ -36,7 +37,8 @@ import { OrderComponent } from './order/order.component'
     ShoppingCartComponent,
     MenuItemComponent,
     ReviewsComponent,
-    OrderComponent
+    OrderComponent,
+    InputComponent
   ],
   imports: [
     BrowserModule,
