45dd9ac32c73929508b86b7127ffa007c41c1a6e
= <devwellington@gmail.com>
2018-07-07 18:49:08 -0300

    Adicionado component radio com property binding

--- angular-projects/meat-app-initial/src/app/app.module.ts
+++ angular-projects/meat-app-initial/src/app/app.module.ts
@@ -22,7 +22,8 @@ import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component'
 
 import { ShoppingCartService } from './restaurant-detail/shopping-cart/shopping-cart.service';
 import { OrderComponent } from './order/order.component';
-import { InputComponent } from './shared/input/input.component'
+import { InputComponent } from './shared/input/input.component';
+import { RadioComponent } from './shared/radio/radio.component'
 
 @NgModule({
   declarations: [
@@ -38,7 +39,8 @@ import { InputComponent } from './shared/input/input.component'
     MenuItemComponent,
     ReviewsComponent,
     OrderComponent,
-    InputComponent
+    InputComponent,
+    RadioComponent
   ],
   imports: [
     BrowserModule,
